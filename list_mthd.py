1fcb06f47c7a355601290a3d0cf8a3fc0f6cb137
harshpaliwal0420 <133878137+harshpaliwal0420@users.noreply.github.com>
2023-10-26 23:57:20 +0530

    Add files via upload

---
+++ list_mthd.py
@@ -0,0 +1,19 @@
+l = [1, 2, 1, 23, 43, 55, 6, 7, 6]
+l.sort()
+l.append(9)
+l.sort(reverse=True)
+ind_l = l.index(43)
+print(ind_l)
+cont_l = l.count(1)
+print(cont_l)
+
+print(l)
+m = l
+m[0] = 89
+print(l)
+l.insert(3, 999)
+print(l)
+
+x = [122, 322, 343, 4454, 5656]
+x.extend(l)
+print(x)
